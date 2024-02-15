d61936d98c958e7319691fd12405897b05a67d8a
O'rolov Shohruh <urolovshohruhilhomivich@gmail.com>
2024-02-15 17:16:45 +0500

    settings change

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-CSRF_TRUSTED_ORIGINS = ['https://blogapi-aq6c.onrender.com', 'https://127.0.0.1:8000', 'https://0.0.0.0:8000']
+
+
+CSRF_TRUSTED_ORIGINS = ['https://blogapi-zerp.onrender.com', 'https://127.0.0.1:8000', 'https://0.0.0.0:8000']
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
